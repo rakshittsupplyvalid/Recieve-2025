5e14e72d96cb9744016a48d7460af66e7a3de6e3
Rakshit Tyagi <rakshit.t@supplyvalid.com>
2025-09-16 15:35:25 +0530

    updated

--- service/api/apiInterceptors.tsx
+++ service/api/apiInterceptors.tsx
@@ -3,7 +3,7 @@ import jscrypto from '../../utils/jscrypto';
 import { retrieveToken } from '../../utils/authUtils';
 
 const apiClient = axios.create({
-  baseURL: 'https://stage-backend-2025.epravaha.com',
+  baseURL: 'https://backend-2025.epravaha.com',
   headers: {
     'Content-Type': 'application/octet-stream',
     'isencrypted': 'true',
